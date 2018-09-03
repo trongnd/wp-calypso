d88be7bed8e5851369708a1a54176e6c813e3385
Dennis Snell <dennis.snell@automattic.com>
2018-09-03 19:24:11 +0100

    test

--- client/state/data-layer/wpcom/me/transactions/index.js
+++ client/state/data-layer/wpcom/me/transactions/index.js
@@ -11,25 +11,33 @@ import { http } from 'state/data-layer/wpcom-http/actions';
 import { dispatchRequestEx } from 'state/data-layer/wpcom-http/utils';
 import { convertToCamelCase as fromApi } from 'state/data-layer/utils';
 import { TRANSACTION_CREATE_REQUEST } from 'state/action-types';
-import { setCreateTransactionResponse, setCreateTransactionError } from 'state/transactions/actions';
+import {
+	setCreateTransactionResponse,
+	setCreateTransactionError,
+} from 'state/transactions/actions';
 
-export const createTransaction = ( action ) => http(
-	{
-		path: '/me/transactions',
-		method: 'POST',
-		apiNamespace: 'rest/v1',
-		query: Object.assign( {}, { http_envelope: 1 }, action.request )
-	},
-	action
-);
+import { registerHandlers } from 'state/data-layer/handler-registry';
 
-export default {
+export const createTransaction = action =>
+	http(
+		{
+			path: '/me/transactions',
+			method: 'POST',
+			apiNamespace: 'rest/v1',
+			query: Object.assign( {}, { http_envelope: 1 }, action.request ),
+		},
+		action
+	);
+
+registerHandlers( 'state/data-layer/wpcom/me/transactions/index.js', {
 	[ TRANSACTION_CREATE_REQUEST ]: [
 		dispatchRequestEx( {
 			fetch: createTransaction,
 			onSuccess: setCreateTransactionResponse,
 			onError: setCreateTransactionError,
-			fromApi
-		} )
-	]
-};
+			fromApi,
+		} ),
+	],
+} );
+
+export default {};
